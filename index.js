39b7d715824ba1bd4b6156b30bedbbaa9556c573
Bianca Raiane <bianca.raiane18@gmail.com>
2022-07-15 16:45:23 -0300

    Api cep done

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 'use strict';
 
-const preencherFormulario = (endereco) => {
-    document.getElementById('endereco').value = endereco.logradouro;
+//by bianca
+
+const limparFormulario = (endereco) => {
+    document.getElementById('endereco').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('cidade').value = '';
+    document.getElementById('estado').value = '';        
 }
+
+
+const preencherFormulario = (endereco) => {   
+    document.getElementById('endereco').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;        
+}
+
+
+const eNumero = (numero) => /^[0-9]+$/.test(numero);
+
+const cepValido = (cep) => cep.length == 8 && eNumero(cep);
 
 const pesquisaCep = async() => {
-    const cep = document.getElementById('cep').value;
-    const url = `http://viacep.com.br/ws/${cep}/json/`;
+    limparFormulario(); 
 
-  
-    const dados = await fetch(url);
-    const endereco = await dados.json();
-    preencherFormulario(endereco);
-}
+    const cep = document.getElementById('cep').value.replace("-","");
+    const url = `http://viacep.com.br/ws/${cep}/json/`;
+    if (cepValido(cep)){  
+        const dados = await  fetch(url);
+        const endereco = await dados.json();
+        if (endereco.hasOwnProperty('erro')){
+            document.getElementById('endereco').value = 'Cep não encontrado!';
+        }else{
+            preencherFormulario(endereco);
+        }
+
+        }else {
+            document.getElementById('endereco').value = 'CEP incorreto!';
+        }
+    }
+        
+          
+
 
 document.getElementById('cep').addEventListener('focusout', pesquisaCep); 
+
